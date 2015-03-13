0b6a413e7ce819e60c182275ff65d15935cdfb24
Tiago <tiago.soares.cfpti@gmail.com>
2015-03-13 13:27:29 +0100

    Ajout classe FileReader

---
+++ NSBDL_Projet/NSBDL_Projet/FileReader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSBDL_Projet
+{
+    /// <summary>
+    /// Reads the csv Student Names List
+    /// </summary>
+    class FileReader
+    {
+
+    }
+}
