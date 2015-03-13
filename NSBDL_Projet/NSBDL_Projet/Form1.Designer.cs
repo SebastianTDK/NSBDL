a07d6154cca900ec445bdcf51f38bcde06a77628
NetuschilD <netuschil.dylan@gmail.com>
2015-03-13 15:55:37 +0100

    Mise à jour de l'interface

--- NSBDL_Projet/NSBDL_Projet/Form1.Designer.cs
+++ NSBDL_Projet/NSBDL_Projet/Form1.Designer.cs
@@ -31,21 +31,20 @@
             this.tbxClassName = new System.Windows.Forms.TextBox();
             this.btnClassName = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.groupBox4 = new System.Windows.Forms.GroupBox();
-            this.groupBox5 = new System.Windows.Forms.GroupBox();
-            this.tbxAddStudentName = new System.Windows.Forms.TextBox();
-            this.btnGenererFichier = new System.Windows.Forms.Button();
             this.lblClassName = new System.Windows.Forms.Label();
-            this.tbxAddStudentFirstname = new System.Windows.Forms.TextBox();
-            this.lblTitleAddStudent = new System.Windows.Forms.Label();
-            this.btnAddStudent = new System.Windows.Forms.Button();
-            this.lblAddStudentName = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnGenererFichier = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.lblAddStudentFirstname = new System.Windows.Forms.Label();
+            this.lblAddStudentName = new System.Windows.Forms.Label();
+            this.btnAddStudent = new System.Windows.Forms.Button();
+            this.lblTitleAddStudent = new System.Windows.Forms.Label();
+            this.tbxAddStudentFirstname = new System.Windows.Forms.TextBox();
+            this.tbxAddStudentName = new System.Windows.Forms.TextBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
             this.groupBox1.SuspendLayout();
-            this.groupBox3.SuspendLayout();
             this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
             // tbxClassName
@@ -77,6 +76,15 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Raphael";
             // 
+            // lblClassName
+            // 
+            this.lblClassName.AutoSize = true;
+            this.lblClassName.Location = new System.Drawing.Point(7, 35);
+            this.lblClassName.Name = "lblClassName";
+            this.lblClassName.Size = new System.Drawing.Size(97, 13);
+            this.lblClassName.TabIndex = 2;
+            this.lblClassName.Text = "Nom de la classe  :";
+            // 
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.btnGenererFichier);
@@ -86,6 +94,16 @@
             this.groupBox2.TabIndex = 3;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Sebastian";
+            // 
+            // btnGenererFichier
+            // 
+            this.btnGenererFichier.Location = new System.Drawing.Point(65, 148);
+            this.btnGenererFichier.Name = "btnGenererFichier";
+            this.btnGenererFichier.Size = new System.Drawing.Size(113, 23);
+            this.btnGenererFichier.TabIndex = 0;
+            this.btnGenererFichier.Text = "Générer fichier";
+            this.btnGenererFichier.UseVisualStyleBackColor = true;
+            this.btnGenererFichier.Click += new System.EventHandler(this.btnGenererFichier_Click);
             // 
             // groupBox3
             // 
@@ -102,68 +120,23 @@
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "François";
             // 
-            // groupBox4
+            // lblAddStudentFirstname
             // 
-            this.groupBox4.Location = new System.Drawing.Point(176, 275);
-            this.groupBox4.Name = "groupBox4";
-            this.groupBox4.Size = new System.Drawing.Size(284, 252);
-            this.groupBox4.TabIndex = 5;
-            this.groupBox4.TabStop = false;
-            this.groupBox4.Text = "Tiago";
+            this.lblAddStudentFirstname.AutoSize = true;
+            this.lblAddStudentFirstname.Location = new System.Drawing.Point(6, 119);
+            this.lblAddStudentFirstname.Name = "lblAddStudentFirstname";
+            this.lblAddStudentFirstname.Size = new System.Drawing.Size(49, 13);
+            this.lblAddStudentFirstname.TabIndex = 5;
+            this.lblAddStudentFirstname.Text = "Prenom :";
             // 
-            // groupBox5
+            // lblAddStudentName
             // 
-            this.groupBox5.Controls.Add(this.btntestDylan);
-            this.groupBox5.Location = new System.Drawing.Point(509, 275);
-            this.groupBox5.Name = "groupBox5";
-            this.groupBox5.Size = new System.Drawing.Size(249, 251);
-            this.groupBox5.TabIndex = 6;
-            this.groupBox5.TabStop = false;
-            this.groupBox5.Text = "Dylan";
-            // 
-            // tbxAddStudentName
-            // lblClassName
-            // 
-            this.tbxAddStudentName.Location = new System.Drawing.Point(67, 56);
-            this.tbxAddStudentName.Name = "tbxAddStudentName";
-            this.tbxAddStudentName.Size = new System.Drawing.Size(100, 20);
-            this.tbxAddStudentName.TabIndex = 0;
-            this.tbxAddStudentName.TextChanged += new System.EventHandler(this.tbxAddStudentName_TextChanged);
-            this.tbxAddStudentName.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbxAddStudentName_KeyPress);
-            // 
-            // tbxAddStudentFirstname
-            this.lblClassName.AutoSize = true;
-            // btnGenererFichier
-            // 
-            this.btnGenererFichier.Location = new System.Drawing.Point(65, 148);
-            this.btnGenererFichier.Name = "btnGenererFichier";
-            this.btnGenererFichier.Size = new System.Drawing.Size(113, 23);
-            this.btnGenererFichier.TabIndex = 0;
-            this.btnGenererFichier.Text = "Générer fichier";
-            this.btnGenererFichier.UseVisualStyleBackColor = true;
-            this.btnGenererFichier.Click += new System.EventHandler(this.btnGenererFichier_Click);
-            // 
-            this.lblClassName.Location = new System.Drawing.Point(7, 35);
-            this.lblClassName.Name = "lblClassName";
-            this.lblClassName.Size = new System.Drawing.Size(97, 13);
-            this.lblClassName.TabIndex = 2;
-            this.lblClassName.Text = "Nom de la classe  :";
-            // 
-            this.tbxAddStudentFirstname.Location = new System.Drawing.Point(67, 116);
-            this.tbxAddStudentFirstname.Name = "tbxAddStudentFirstname";
-            this.tbxAddStudentFirstname.Size = new System.Drawing.Size(100, 20);
-            this.tbxAddStudentFirstname.TabIndex = 1;
-            this.tbxAddStudentFirstname.TextChanged += new System.EventHandler(this.tbxAddStudentFirstname_TextChanged);
-            this.tbxAddStudentFirstname.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbxAddStudentFirstname_KeyPress);
-            // 
-            // lblTitleAddStudent
-            // 
-            this.lblTitleAddStudent.AutoSize = true;
-            this.lblTitleAddStudent.Location = new System.Drawing.Point(6, 16);
-            this.lblTitleAddStudent.Name = "lblTitleAddStudent";
-            this.lblTitleAddStudent.Size = new System.Drawing.Size(84, 13);
-            this.lblTitleAddStudent.TabIndex = 2;
-            this.lblTitleAddStudent.Text = "Ajouter un élève";
+            this.lblAddStudentName.AutoSize = true;
+            this.lblAddStudentName.Location = new System.Drawing.Point(6, 59);
+            this.lblAddStudentName.Name = "lblAddStudentName";
+            this.lblAddStudentName.Size = new System.Drawing.Size(35, 13);
+            this.lblAddStudentName.TabIndex = 4;
+            this.lblAddStudentName.Text = "Nom :";
             // 
             // btnAddStudent
             // 
@@ -176,30 +149,47 @@
             this.btnAddStudent.UseVisualStyleBackColor = true;
             this.btnAddStudent.Click += new System.EventHandler(this.btnAddStudent_Click);
             // 
-            // lblAddStudentName
+            // lblTitleAddStudent
             // 
-            this.lblAddStudentName.AutoSize = true;
-            this.lblAddStudentName.Location = new System.Drawing.Point(6, 59);
-            this.lblAddStudentName.Name = "lblAddStudentName";
-            this.lblAddStudentName.Size = new System.Drawing.Size(35, 13);
-            this.lblAddStudentName.TabIndex = 4;
-            this.lblAddStudentName.Text = "Nom :";
+            this.lblTitleAddStudent.AutoSize = true;
+            this.lblTitleAddStudent.Location = new System.Drawing.Point(6, 16);
+            this.lblTitleAddStudent.Name = "lblTitleAddStudent";
+            this.lblTitleAddStudent.Size = new System.Drawing.Size(84, 13);
+            this.lblTitleAddStudent.TabIndex = 2;
+            this.lblTitleAddStudent.Text = "Ajouter un élève";
             // 
-            // lblAddStudentFirstname
+            // tbxAddStudentFirstname
             // 
-            this.lblAddStudentFirstname.AutoSize = true;
-            this.lblAddStudentFirstname.Location = new System.Drawing.Point(6, 119);
-            this.lblAddStudentFirstname.Name = "lblAddStudentFirstname";
-            this.lblAddStudentFirstname.Size = new System.Drawing.Size(49, 13);
-            this.lblAddStudentFirstname.TabIndex = 5;
-            this.lblAddStudentFirstname.Text = "Prenom :";
+            this.tbxAddStudentFirstname.Location = new System.Drawing.Point(67, 116);
+            this.tbxAddStudentFirstname.Name = "tbxAddStudentFirstname";
+            this.tbxAddStudentFirstname.Size = new System.Drawing.Size(100, 20);
+            this.tbxAddStudentFirstname.TabIndex = 1;
+            this.tbxAddStudentFirstname.TextChanged += new System.EventHandler(this.tbxAddStudentFirstname_TextChanged);
+            this.tbxAddStudentFirstname.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbxAddStudentFirstname_KeyPress);
+            // 
+            // tbxAddStudentName
+            // 
+            this.tbxAddStudentName.Location = new System.Drawing.Point(67, 56);
+            this.tbxAddStudentName.Name = "tbxAddStudentName";
+            this.tbxAddStudentName.Size = new System.Drawing.Size(100, 20);
+            this.tbxAddStudentName.TabIndex = 0;
+            this.tbxAddStudentName.TextChanged += new System.EventHandler(this.tbxAddStudentName_TextChanged);
+            this.tbxAddStudentName.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbxAddStudentName_KeyPress);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Location = new System.Drawing.Point(176, 275);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(284, 252);
+            this.groupBox4.TabIndex = 5;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Tiago";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1186, 538);
-            this.Controls.Add(this.groupBox5);
             this.Controls.Add(this.groupBox4);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
@@ -208,9 +198,8 @@
             this.Text = "Form1";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
-            this.groupBox3.ResumeLayout(false);
-            this.groupBox5.ResumeLayout(false);
             this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
             this.groupBox3.PerformLayout();
             this.ResumeLayout(false);
 
@@ -224,7 +213,6 @@
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.GroupBox groupBox3;
         private System.Windows.Forms.GroupBox groupBox4;
-        private System.Windows.Forms.GroupBox groupBox5;
         private System.Windows.Forms.TextBox tbxAddStudentFirstname;
         private System.Windows.Forms.Button btnGenererFichier;
         private System.Windows.Forms.Label lblClassName;
