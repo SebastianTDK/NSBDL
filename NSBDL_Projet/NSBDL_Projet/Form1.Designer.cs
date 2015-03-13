a98c76d97af701b42ee7d73c46c1016e6c012340
Franfrandudu <francois.duvernaycfpt@gmail.com>
2015-03-13 13:38:29 +0100

    Ajout des composants pour ajouter un élève

--- NSBDL_Projet/NSBDL_Projet/Form1.Designer.cs
+++ NSBDL_Projet/NSBDL_Projet/Form1.Designer.cs
@@ -35,7 +35,14 @@
             this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.groupBox4 = new System.Windows.Forms.GroupBox();
             this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.tbxAddStudentName = new System.Windows.Forms.TextBox();
+            this.tbxAddStudentFirstname = new System.Windows.Forms.TextBox();
+            this.lblTitleAddStudent = new System.Windows.Forms.Label();
+            this.btnAddStudent = new System.Windows.Forms.Button();
+            this.lblAddStudentName = new System.Windows.Forms.Label();
+            this.lblAddStudentFirstname = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
             // textBox1
@@ -77,6 +84,12 @@
             // 
             // groupBox3
             // 
+            this.groupBox3.Controls.Add(this.lblAddStudentFirstname);
+            this.groupBox3.Controls.Add(this.lblAddStudentName);
+            this.groupBox3.Controls.Add(this.btnAddStudent);
+            this.groupBox3.Controls.Add(this.lblTitleAddStudent);
+            this.groupBox3.Controls.Add(this.tbxAddStudentFirstname);
+            this.groupBox3.Controls.Add(this.tbxAddStudentName);
             this.groupBox3.Location = new System.Drawing.Point(670, 18);
             this.groupBox3.Name = "groupBox3";
             this.groupBox3.Size = new System.Drawing.Size(290, 250);
@@ -102,6 +115,56 @@
             this.groupBox5.TabStop = false;
             this.groupBox5.Text = "Dylan";
             // 
+            // tbxAddStudentName
+            // 
+            this.tbxAddStudentName.Location = new System.Drawing.Point(67, 56);
+            this.tbxAddStudentName.Name = "tbxAddStudentName";
+            this.tbxAddStudentName.Size = new System.Drawing.Size(100, 20);
+            this.tbxAddStudentName.TabIndex = 0;
+            // 
+            // tbxAddStudentFirstname
+            // 
+            this.tbxAddStudentFirstname.Location = new System.Drawing.Point(67, 116);
+            this.tbxAddStudentFirstname.Name = "tbxAddStudentFirstname";
+            this.tbxAddStudentFirstname.Size = new System.Drawing.Size(100, 20);
+            this.tbxAddStudentFirstname.TabIndex = 1;
+            // 
+            // lblTitleAddStudent
+            // 
+            this.lblTitleAddStudent.AutoSize = true;
+            this.lblTitleAddStudent.Location = new System.Drawing.Point(6, 16);
+            this.lblTitleAddStudent.Name = "lblTitleAddStudent";
+            this.lblTitleAddStudent.Size = new System.Drawing.Size(84, 13);
+            this.lblTitleAddStudent.TabIndex = 2;
+            this.lblTitleAddStudent.Text = "Ajouter un élève";
+            // 
+            // btnAddStudent
+            // 
+            this.btnAddStudent.Location = new System.Drawing.Point(67, 174);
+            this.btnAddStudent.Name = "btnAddStudent";
+            this.btnAddStudent.Size = new System.Drawing.Size(75, 23);
+            this.btnAddStudent.TabIndex = 3;
+            this.btnAddStudent.Text = "Ajouter";
+            this.btnAddStudent.UseVisualStyleBackColor = true;
+            // 
+            // lblAddStudentName
+            // 
+            this.lblAddStudentName.AutoSize = true;
+            this.lblAddStudentName.Location = new System.Drawing.Point(6, 59);
+            this.lblAddStudentName.Name = "lblAddStudentName";
+            this.lblAddStudentName.Size = new System.Drawing.Size(35, 13);
+            this.lblAddStudentName.TabIndex = 4;
+            this.lblAddStudentName.Text = "Nom :";
+            // 
+            // lblAddStudentFirstname
+            // 
+            this.lblAddStudentFirstname.AutoSize = true;
+            this.lblAddStudentFirstname.Location = new System.Drawing.Point(6, 119);
+            this.lblAddStudentFirstname.Name = "lblAddStudentFirstname";
+            this.lblAddStudentFirstname.Size = new System.Drawing.Size(49, 13);
+            this.lblAddStudentFirstname.TabIndex = 5;
+            this.lblAddStudentFirstname.Text = "Prenom :";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -116,6 +179,8 @@
             this.Text = "Form1";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -129,6 +194,12 @@
         private System.Windows.Forms.GroupBox groupBox3;
         private System.Windows.Forms.GroupBox groupBox4;
         private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.TextBox tbxAddStudentFirstname;
+        private System.Windows.Forms.TextBox tbxAddStudentName;
+        private System.Windows.Forms.Label lblAddStudentFirstname;
+        private System.Windows.Forms.Label lblAddStudentName;
+        private System.Windows.Forms.Button btnAddStudent;
+        private System.Windows.Forms.Label lblTitleAddStudent;
     }
 }
 
