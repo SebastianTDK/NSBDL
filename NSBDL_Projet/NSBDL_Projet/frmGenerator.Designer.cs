21fe7439c12e3705d7f258d3648cf304f0e0ded8
Raphael B <Bonatti.raphael@gmail.com>
2015-03-27 16:04:15 +0100

    Erreur BtnDelete

--- NSBDL_Projet/NSBDL_Projet/frmGenerator.Designer.cs
+++ NSBDL_Projet/NSBDL_Projet/frmGenerator.Designer.cs
@@ -191,7 +191,6 @@
             this.btnDelete.TabIndex = 7;
             this.btnDelete.Text = "Effacer";
             this.btnDelete.UseVisualStyleBackColor = true;
-            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
             // 
             // frmGenerator
             // 
